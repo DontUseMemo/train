df0ca9740b2406d4f50e8d300c54370485779e7e
DontUseMemo <nann76554@gmail.com>
2022-08-19 17:21:43 +0900

    날짜 업데이트 어노테이션 적용

--- Shoppingmall/src/main/java/com/example/shoppingmall/entity/base/BaseTimeEntity.java
+++ Shoppingmall/src/main/java/com/example/shoppingmall/entity/base/BaseTimeEntity.java
@@ -4,7 +4,9 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -12,6 +14,7 @@ import java.util.Date;
 
 @Setter
 @Getter
+@EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
 public abstract class BaseTimeEntity {
 
