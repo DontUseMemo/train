eb0d0b9b7c33d77b7a389f39c15bc22b9cf1bc12
DontUseMemo <nann76554@gmail.com>
2022-08-31 17:35:32 +0900

    크롬드랑버 안 됨

--- untitled/Scraping.py
+++ untitled/Scraping.py
@@ -1,3 +1,20 @@
-from selenium import webdriver
+import subprocess
 
-driver = webdriver.Chrome("D:/chromedriver.exe")
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+driver = webdriver.Chrome(r'D:/chromedriver.exe')
+
+subprocess.Popen(r'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe'
+                 r' --remote-debugging-port=9222 --user-data-dir="C:\chrometemp"')
+
+option = Options()
+option.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
+
+chrome_ver = chromedriver_autoinstaller.get_chrome_version().split('.')[0]
+try:
+    driver = webdriver.Chrome(f'./{chrome_ver}/chromedriver.exe', options=option)
+except:
+    chromedriver_autoinstaller.install(True)
+    driver = webdriver.Chrome(f'./{chrome_ver}/chromedriver.exe', options=option)
+driver.implicitly_wait(10)
